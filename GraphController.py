4a45bfea26351b8a159793eb6d837c23c0784cc6
frlyruc <frly@ruc.dk>
2021-04-13 15:27:45 +0200

    Update

--- GraphController.py
+++ GraphController.py
@@ -3,7 +3,6 @@ from matplotlib.widgets import Cursor, Button
 
 class GraphController:
     def __init__(self): #Constructor
-        self.fig, self.ax = plt.subplots() #Init the figure and init a subplot in figure
         self.p, = plt.plot(0,0,'o') #Plot origin
         self.cursor = Cursor(self.ax, horizOn=True, vertOn = True, color = 'red', linewidth = 2.0) ##Cursor coordinates + crosshair
         self.coords = [] ##Init coord-array
@@ -13,7 +12,7 @@ class GraphController:
         plt.show() 
     
 
-    def onclick(self,event): #Onclick event that adds a vertex to the array and plots it aswell
+    def onclick(self,event): #Onclick event that adds a vertex to the array and plots it aswell.
         self.coords.append([event.xdata, event.ydata])
         self.ax.plot(event.xdata,event.ydata,'o')
 
