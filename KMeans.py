67c1e79631160f91f1d999f9db04a4a9a3c7ac9c
EmilSejerP <71255873+EmilSejerP@users.noreply.github.com>
2021-04-17 10:58:56 +0200

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -1,13 +1,25 @@
 from sklearn.cluster import KMeans as K
-import numpy as np
 
-class KMeans:
-    def __init__(self, coords):
-        self.X = np.array(coords)
-        self.kmeans = K(n_clusters=2, random_state=0).fit(self.X)
-        
-    def update(self):
-        
+class KMeansAlgorithm:
+    def __init__(self):
+        self.arrayOfStores = []
 
-    def returnKMeans(self):
-        return self.kmeans
+    #Call to update array for kmeans
+    def updateStoreArray(self,coords):
+        self.arrayOfStores.append(coords)
+
+    #Calculates KMeans if there is more than 4 stores in the array
+    def calculateKMeans(self):
+        if len(self.arrayOfStores) > 4:
+            kmeans = K(n_clusters=2)
+            kmeans.fit(self.arrayOfStores)
+            clusters = kmeans.cluster_centers_
+            cluster1 = [clusters[0][0],clusters[0][1]]
+            cluster2 = [clusters[1][0],clusters[1][1]]
+            return cluster1, cluster2
+        else:
+            print("Need more stores to calculate KMeans")
+            return 0
+
+    #def nodes_to_category(self,node):
+    #    for i in
